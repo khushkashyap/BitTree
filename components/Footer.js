583a31833fc6e40bf6695e93add564bc4c580cb0
khushkashyap <130690679+khushkashyap@users.noreply.github.com>
2026-04-08 21:03:32 +0530

    fix: Improve layout and styling of Footer component buttons for better alignment and responsiveness

--- components/Footer.js
+++ components/Footer.js
@@ -21,15 +21,15 @@ const Footer = () => {
         <footer className="p-4 sm:p-6 md:p-10 bg-[#502274]">
           {/* Footer Content */}
           <div className="flex flex-col sm:flex-row justify-between max-w-6xl px-4 sm:px-6 md:px-20 py-8 md:py-14 rounded-3xl mx-auto gap-6 sm:gap-8 md:gap-12 text-xs sm:text-sm md:text-base bg-white items-center md:items-center">
-            <div className="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
+            <div className="flex flex-col items-center sm:flex-row gap-2 w-full sm:w-auto">
               <SignedOut>
               <SignInButton>
-                <button className="login bg-gray-200 px-4 sm:px-6 py-2 sm:py-4 rounded-lg font-medium text-xs sm:text-sm hover:bg-gray-300 transition-colors">
+                <button className="login bg-gray-200 w-fit px-4 sm:px-6 py-2 sm:py-4 rounded-lg font-medium text-xs sm:text-sm hover:bg-gray-300 transition-colors">
                   Log in
                 </button>
               </SignInButton>
               <SignUpButton>
-                <button className="signup bg-gray-800 text-white px-6 sm:px-8 py-2 sm:py-4 font-bold rounded-full text-xs sm:text-sm hover:bg-gray-900 transition-colors">
+                <button className="signup bg-gray-800 w-fit text-white px-6 sm:px-8 py-2 sm:py-4 font-bold rounded-full text-xs sm:text-sm hover:bg-gray-900 transition-colors">
                   Get Started for free
                 </button>
               </SignUpButton>
